87fcb25dbd0062e1ba6d814a7fea5c270990f4f0
Baby goku <webdevgoku@gmail.com>
2020-03-02 14:46:02 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,12 +1,2 @@
 board = ["X", " ", " ", " ", " ", " ", " ", " ", "O"]
 
-def position_taken?(board,index)
-  if(board[index]==" "||""||nil)
-    false
-  elsif(board[index] == "X" || "O")
-    true;
-  else
-    true;
-  end
-end
-
